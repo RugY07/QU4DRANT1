d6812d37af2f4787ae0233f749e614990951518f
Rugweda Yende <yrugwa240407@gmail.com>
2025-08-02 19:59:08 +0530

    fix: force lowercase filename for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./pages/index";
-import Index from './pages/index';
+import Page from './pages/index';
 import './index.css'; // Assuming your global styles are here
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
